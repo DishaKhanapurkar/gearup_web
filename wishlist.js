c8ebc28d6c1ce978d9a19a37c16a5028afec15a2
Disha Khanapurkar <86222578+DishaKhanapurkar@users.noreply.github.com>
2022-08-06 14:05:53 +0530

    Add files via upload

---
+++ wishlist.js
@@ -0,0 +1,97 @@
+function myFunction1() {
+    document.getElementById("demo1").innerHTML = "BMX Cycle";
+}
+function myFunction2() {
+  document.getElementById("demo2").innerHTML = "Hydrid Cycle";
+  
+}
+function myFunction3() {
+    document.getElementById("demo3").innerHTML = "MTB Cycle";
+    
+  }
+  function myFunction4() {
+    document.getElementById("demo4").innerHTML = "v10 Carbon BMX Cycle";
+    
+  }
+  function myFunction5() {
+    document.getElementById("demo5").innerHTML = "Cardic Speed Hybrid Cycle";
+    
+  }
+  function myFunction6() {
+    document.getElementById("demo6").innerHTML = "Blue MTB";
+    
+  };
+  function myFunction7() {
+    document.getElementById("demo7").innerHTML = "Jaugar MTB Cycle";
+    
+  }
+  function myFunction8() {
+    document.getElementById("demo8").innerHTML = "HoTrack MTB Cycle";
+    
+  }
+  function myFunction9() {
+    document.getElementById("demo9").innerHTML = "Vector MTB";
+    
+  }
+  function myFunction10() {
+    document.getElementById("demo10").innerHTML = "Hybrid2 Cycle";
+    
+  }
+  function myFunction11() {
+    document.getElementById("demo11").innerHTML = "Women Bicycle";
+    
+  }
+  function myFunction12() {
+    document.getElementById("demo12").innerHTML = "Bravo Hybrid Bicycle";
+    
+  }
+  function myFunction13() {
+    document.getElementById("demo13").innerHTML = "New Hybrid";
+    
+  }
+  function myFunction14() {
+    document.getElementById("demo14").innerHTML = "MTB Kids Special";
+    
+  }
+  function myFunction15() {
+    document.getElementById("demo15").innerHTML = "BMX Ultra";
+    
+  }
+  function myFunction16() {
+    document.getElementById("demo16").innerHTML = "Yellow Cycle";
+    
+  }
+  function myFunction17() {
+    document.getElementById("demo17").innerHTML = "MTB Kids";
+    
+  }
+  function myFunction18() {
+    document.getElementById("demo18").innerHTML = "Hybrid XP";
+    
+  }
+  function myFunction19() {
+    document.getElementById("demo19").innerHTML = "Helmet";
+    
+  }
+  function myFunction20() {
+    document.getElementById("demo20").innerHTML = "Bottle Holder";
+    
+  }
+  function myFunction21() {
+    document.getElementById("demo21").innerHTML = "Saddle Bag";
+    
+  }
+  function myFunction22() {
+    document.getElementById("demo22").innerHTML = "Mask";
+    
+  }
+  function myFunction23() {
+    document.getElementById("demo23").innerHTML = "Bottle";
+    
+  }
+  function myFunction24() {
+    document.getElementById("demo24").innerHTML = "Bike Lock";
+    
+  }
+
+
